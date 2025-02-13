e9d387f2b450e53b8d3f9b3de22f47ba5a81a070
Tomoki-0526 <eusford_0526@outlook.com>
2025-02-13 12:33:42 +0800

    恢复只生成latent code

--- inference.py
+++ inference.py
@@ -17,8 +17,8 @@ from dataset import SurfData
 from trainer import EdgeVAETrainer
 from dataset import EdgeData
 
-M = 30      # max number of faces in a cad model
-N = M*20    # max number of edges of a face in a cad model
+M = 30  # max number of faces in a cad model
+N = 20  # max number of edges of a face in a cad model
 
 def run(args):
     if args.option == 'surface':
@@ -28,117 +28,19 @@ def run(args):
         vae = SurfVAETrainer(args, train_dataset, val_dataset, test_dataset)
 
         print('Start surface inference...')
-        z = vae.inference_latent()
-
-        grouped_z = {
-            'train': [],
-            'val': [],
-            'test': []
-        }
-        grouped_mask = {
-            'train': [],
-            'val': [],
-            'test': []
-        }
-        last_idx = 0
-        for k in train_dataset.group:
-            valid_z = z[last_idx:last_idx+k]
-            zero_z = torch.zeros((M-k, 3, 4, 4)).to(z.device)
-            integ_z = torch.cat([valid_z, zero_z], dim=0)
-            grouped_z['train'].append(integ_z)
-            mask = torch.cat([
-                torch.ones(k, dtype=torch.bool).to(z.device),
-                torch.zeros(M-k, dtype=torch.bool).to(z.device)
-            ])
-            grouped_mask['train'].append(mask)
-            last_idx += k
-
-        last_idx = 0
-        for k in val_dataset.group:
-            valid_z = z[last_idx:last_idx+k]
-            zero_z = torch.zeros((M-k, 3, 4, 4)).to(z.device)
-            integ_z = torch.cat([valid_z, zero_z], dim=0)
-            grouped_z['val'].append(integ_z)
-            mask = torch.cat([
-                torch.ones(k, dtype=torch.bool).to(z.device),
-                torch.zeros(M-k, dtype=torch.bool).to(z.device)
-            ])
-            grouped_mask['val'].append(mask)
-            last_idx += k
-
-        last_idx = 0
-        for k in test_dataset.group:
-            valid_z = z[last_idx:last_idx+k]
-            zero_z = torch.zeros((M-k, 3, 4, 4)).to(z.device)
-            integ_z = torch.cat([valid_z, zero_z], dim=0)
-            grouped_z['test'].append(integ_z)
-            mask = torch.cat([
-                torch.ones(k, dtype=torch.bool).to(z.device),
-                torch.zeros(M-k, dtype=torch.bool).to(z.device)
-            ])
-            grouped_mask['test'].append(mask)
-            last_idx += k
+        z = vae.inference_latent() 
 
     else:
         assert args.option == 'edge', 'please choose between surface or edge'
         train_dataset = EdgeData(args.data, args.train_list, aug=args.data_aug)
         val_dataset = EdgeData(args.data, args.val_list, validate=True, aug=False)
         test_dataset = EdgeData(args.data, args.test_list, test=True, aug=False)
-        vae = EdgeVAETrainer(args, train_dataset, val_dataset)
+        vae = EdgeVAETrainer(args, train_dataset, val_dataset, test_dataset)
 
         print('Start edge inference...')
         z = vae.inference_latent()
 
-        grouped_z = {
-            'train': [],
-            'val': [],
-            'test': []
-        }
-        grouped_mask = {
-            'train': [],
-            'val': [],
-            'test': []
-        }
-        last_idx = 0
-        for k in train_dataset.group:
-            valid_z = z[last_idx:last_idx+k]
-            zero_z = torch.zeros((N-k, 3, 4, 4)).to(z.device)
-            integ_z = torch.cat([valid_z, zero_z], dim=0)
-            grouped_z['train'].append(integ_z)
-            mask = torch.cat([
-                torch.ones(k, dtype=torch.bool).to(z.device),
-                torch.zeros(N-k, dtype=torch.bool).to(z.device)
-            ])
-            grouped_mask['train'].append(mask)
-            last_idx += k
-
-        last_idx = 0
-        for k in val_dataset.group:
-            valid_z = z[last_idx:last_idx+k]
-            zero_z = torch.zeros((N-k, 3, 4, 4)).to(z.device)
-            integ_z = torch.cat([valid_z, zero_z], dim=0)
-            grouped_z['val'].append(integ_z)
-            mask = torch.cat([
-                torch.ones(k, dtype=torch.bool).to(z.device),
-                torch.zeros(N-k, dtype=torch.bool).to(z.device)
-            ])
-            grouped_mask['val'].append(mask)
-            last_idx += k
-
-        last_idx = 0
-        for k in test_dataset.group:
-            valid_z = z[last_idx:last_idx+k]
-            zero_z = torch.zeros((N-k, 3, 4, 4)).to(z.device)
-            integ_z = torch.cat([valid_z, zero_z], dim=0)
-            grouped_z['test'].append(integ_z)
-            mask = torch.cat([
-                torch.ones(k, dtype=torch.bool).to(z.device),
-                torch.zeros(N-k, dtype=torch.bool).to(z.device)
-            ])
-            grouped_mask['test'].append(mask)
-            last_idx += k
-
-    return grouped_z
+    return z
 
 if __name__ == "__main__":
     run(args)
